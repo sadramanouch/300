222da04386d233752045a248ccf00dedaf01b041
Ethan MacDonald <jem21@sfu.ca>
2024-03-24 13:02:23 -0700

    increased max nodes and max heads

--- A3/list.h
+++ A3/list.h
@@ -35,11 +35,11 @@ struct List_s{
 
 // Maximum number of unique lists the system can support
 // (You may modify this, but reset the value to 10 when handing in your assignment)
-#define LIST_MAX_NUM_HEADS 10
+#define LIST_MAX_NUM_HEADS 100
 
 // Maximum total number of nodes (statically allocated) to be shared across all lists
 // (You may modify this, but reset the value to 100 when handing in your assignment)
-#define LIST_MAX_NUM_NODES 100
+#define LIST_MAX_NUM_NODES 1000
 
 // General Error Handling:
 // Client code is assumed never to call these functions with a NULL List pointer, or 
